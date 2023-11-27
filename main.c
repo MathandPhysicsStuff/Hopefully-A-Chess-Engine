cd2bb2e1df27945a3565991cee7a45617376c288
Your Name <you@example.com>
2023-11-27 13:37:25 -0500

    more set up

--- main.c
+++ main.c
@@ -1,8 +1,76 @@
+#include <stdio.h>
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
 #include "functions.h"
 
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+const int SCREEN_WIDTH = 860;
+const int SCREEN_HEIGHT = 640;
 
 int main()
 {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) printf("SDL2 video failed to initialize: \n", SDL_GetError());
+    else printf("System is ready to go!\n");
 
-    return 0;
+    window = SDL_CreateWindow("Hello SDL",
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    
+    renderer = SDL_CreateRenderer(window,
+                                  -1,
+                                  SDL_RENDERER_ACCELERATED);
+
+    SDL_bool running = SDL_TRUE;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = SDL_FALSE;
+                break;
+            }
+
+            if (event.type == SDL_KEYDOWN)
+            {
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        running = SDL_FALSE;
+                        break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(100);
+    }
+    
+    //freeing stuff 
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
